9d8319128845f23e86a8e954f13c6749a645d3f6
Jason Regnier <jason.regnier4@gmail.com>
2023-12-17 21:18:47 -0500

    Day 12 Part 1

---
+++ 2023/Problem12/Program.cs
@@ -0,0 +1,12 @@
+﻿using AocUtilities;
+
+using Problem12;
+
+var arguments = Environment.GetCommandLineArgs();
+Console.WriteLine($"Input file: {arguments[1]}");
+Console.WriteLine();
+
+var conditionRecords = new ConditionRecords();
+
+Runner.RunCodeWithTimer(1, () => Console.WriteLine($"Result: {conditionRecords.SolvePart1(arguments[1])}"));
+Runner.RunCodeWithTimer(2, () => Console.WriteLine($"Result: {conditionRecords.SolvePart2(arguments[1])}"));
---
+++ 2023/Problem12/RecordItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem12;
+
+internal record RecordItem
+{
+    public RecordItem(string config, List<int> groupSequence)
+    {
+        Config = config;
+        GroupSequence = groupSequence;
+    }
+
+    public string Config { get; private set; }
+
+    public List<int> GroupSequence { get; private set; }
+
+    public void Unfold()
+    {
+        var configs = Enumerable.Range(0, 5).Select(_ => (cfg:Config, gs: GroupSequence.ToList())).ToList();
+        Config = string.Join("?", configs.Select(x => x.cfg));
+
+        GroupSequence.Clear();
+        configs.ForEach(x => GroupSequence.AddRange(x.gs));
+    }
+}
